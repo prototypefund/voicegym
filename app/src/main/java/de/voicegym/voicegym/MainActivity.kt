c8883326940690678312c19e625df4176e2b7998
Saskia Gennrich <saskia.gennrich@posteo.de>
2018-04-06 17:07:58 +0200

    pre ktlint

--- app/src/main/java/de/voicegym/voicegym/MainActivity.kt
+++ app/src/main/java/de/voicegym/voicegym/MainActivity.kt
@@ -1,14 +1,33 @@
 package de.voicegym.voicegym
 
-import android.support.v7.app.AppCompatActivity
+import android.media.MediaRecorder
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val recorder = MediaRecorder()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recordButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Holst du Bier?", Toast.LENGTH_SHORT).show()
+
+            recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            recorder.setOutputFile(PATH_NAME);
+            recorder.prepare();
+            recorder.start();   // Recording is now started
+
+            recorder.stop();
+            recorder.reset();   // You can reuse the object by going back to setAudioSource() step
+            recorder.release(); // Now the object cannot be reused
+        }
 
         // Example of a call to a native method
         sample_text.text = stringFromJNI()
